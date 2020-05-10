53c576a9b4624de865d9cbdc9ddf644d650fa215
firedream89 <briand-kevin@hotmail.fr>
2020-05-10 15:47:22 +0200

    Option menu + auto update data
    Add option menu with settings
    Add auto update data from server(every 30 sec)
    Add receipt data from consumption for this year
    Change textColor

--- app/src/main/java/Domoserv_Pi/domoserv_android/MainActivity.kt
+++ app/src/main/java/Domoserv_Pi/domoserv_android/MainActivity.kt
@@ -8,12 +8,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.view.Window
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 import kotlin.math.roundToInt
+
 
 enum class Zone { unused, Z1, Z2 }
 
@@ -26,6 +30,8 @@ class MainActivity : AppCompatActivity() {
     private var indoorTemp = List(3) {""}
     private var outdoorTemp = List(3) {""}
     private var frostFree = 0
+    private var timerUpdate = Timer()
+    private var updatePeriod: Long = 30000
 
     inner class WebSocket() : WebSock() {
         override fun onMessage(webSocket: okhttp3.WebSocket, text: String) {
@@ -60,6 +66,7 @@ class MainActivity : AppCompatActivity() {
             val endDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
             val dateYear = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy")) + "-01-01"
             ws.send(getString(R.string.getDataEnergy).replace("{date}",date).replace("{endDate}",endDate))
+            ws.send(getString(R.string.getDataEnergy).replace("{date}",dateYear).replace("{endDate}",endDate))
             ws.send(getString(R.string.getFrostFree))
         }
     }
@@ -67,7 +74,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         stateList = listOf(getString(R.string.comfort),getString(R.string.eco),getString(R.string.frostFree))
         modeList = listOf(getString(R.string.auto), getString(R.string.semiAuto), getString(R.string.manual))
 
@@ -82,9 +88,44 @@ class MainActivity : AppCompatActivity() {
         stateLayout.setOnClickListener {
             showHeaterDialog()
         }
+        //define temp click listener
         val tempLayout = findViewById<LinearLayout>(R.id.btnTemp)
         tempLayout.setOnClickListener {
             showTemperatureDialog()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_activity_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            // launch settings activity
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        when(hasFocus) {
+            true -> {
+                timerUpdate = Timer()
+                timerUpdate.scheduleAtFixedRate(object : TimerTask() {
+                    override fun run() {
+                        ws.startUpdate()
+                    }
+                }, 0, updatePeriod)
+            }
+            false -> {
+                println("StopTimer")
+                timerUpdate.cancel()
+                timerUpdate.purge()
+            }
         }
     }
 
@@ -133,7 +174,6 @@ class MainActivity : AppCompatActivity() {
                     ws.startFrostFree(frostFreeEdit.text.toString().toInt())
                 }
             }
-            ws.startUpdate()
             dialog.dismiss()
         }
         dialog.show()
@@ -158,7 +198,7 @@ class MainActivity : AppCompatActivity() {
 
     fun updateField(data: String) {
         println(data)
-        when(data.split("=").first()) {
+        when(data.split("=").first().split(";;").first()) {
             getString(R.string.getZ1State) -> findViewById<TextView>(R.id.stateZ1).text = stateList[data.split("=").last().toInt()]
             getString(R.string.getZ2State) -> findViewById<TextView>(R.id.stateZ2).text = stateList[data.split("=").last().toInt()]
             getString(R.string.getZ1Mode) -> findViewById<TextView>(R.id.modeZ1).text = modeList[data.split("=").last().toInt()]
@@ -185,18 +225,26 @@ class MainActivity : AppCompatActivity() {
                     findViewById<TextView>(R.id.tempExtActual).text = actual
                 }
             }
-            getString(R.string.getDataEnergy).split(";").first() -> {
+            getString(R.string.getDataEnergy).split(";;").first() -> {
                 val all = data.split("=").last().split("\r").toMutableList()
                 all.removeAt(all.count() - 1)
                 var daily = 0
                 for (value in all) {
                     daily += value.split("|").last().toInt()
                 }
-
                 val dailyCons = (daily / 10.toDouble()).roundToInt() / 100.toDouble()
                 val dailyCost = (daily / 10 * 0.1781).roundToInt() / 100.toDouble()
-                findViewById<TextView>(R.id.dailyCons).text = dailyCons.toString()
-                findViewById<TextView>(R.id.dailyCost).text = dailyCost.toString()
+
+                val date = data.split("=").first().split(";").last().split(":")
+                println(date)
+                if(date.first() != date.last()) {
+                    findViewById<TextView>(R.id.yearCons).text = dailyCons.toString()
+                    findViewById<TextView>(R.id.yearCost).text = dailyCost.toString()
+                }
+                else {
+                    findViewById<TextView>(R.id.dailyCons).text = dailyCons.toString()
+                    findViewById<TextView>(R.id.dailyCost).text = dailyCost.toString()
+                }
             }
             getString(R.string.getFrostFree) -> frostFree = data.split("=").last().toInt()
         }
@@ -244,7 +292,6 @@ class MainActivity : AppCompatActivity() {
                     startActivityForResult(intent, 0)
                 } else {
                     Toast.makeText(this, getString(R.string.connected), Toast.LENGTH_SHORT).show()
-                    ws.startUpdate()
                 }
             }
         }
